3d46347110e89e0e7f76153e0a44404083692d6f
Dagasfoka <gmelo.loredo@gmail.com>
2026-03-12 15:43:29 -0300

    fix websocket

--- backend/main.py
+++ backend/main.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from pathlib import Path
+
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+# servir arquivos estáticos
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 players = []
+
 @app.get("/")
 async def get_index():
     return HTMLResponse(Path("static/index.html").read_text())
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -27,8 +31,11 @@ async def websocket_endpoint(websocket: WebSocket):
 
             print("Mensagem recebida:", data)
 
-            for player in players:
-                await player.send_text(data)
+            for player in players.copy():
+                try:
+                    await player.send_text(data)
+                except:
+                    players.remove(player)
 
     except:
         players.remove(websocket)
