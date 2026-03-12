43104963f2bb37b461c84dae6dd12f317d822348
Dagasfoka <gmelo.loredo@gmail.com>
2026-03-12 15:05:16 -0300

    Primeiro commit do TCC Polis

---
+++ backend/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from pathlib import Path
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+players = []
+@app.get("/")
+async def get_index():
+    return HTMLResponse(Path("static/index.html").read_text())
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+
+    print("Novo jogador tentando conectar")
+
+    await websocket.accept()
+    players.append(websocket)
+
+    print("Jogador conectado. Total:", len(players))
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            print("Mensagem recebida:", data)
+
+            for player in players:
+                await player.send_text(data)
+
+    except:
+        players.remove(websocket)
+        print("Jogador desconectado")
---
+++ static/script.js
@@ -0,0 +1,24 @@
+const socket = new WebSocket("ws://" + location.host + "/ws")
+
+// quando conecta
+socket.onopen = function() {
+    console.log("Conectado ao servidor")
+    alert("Conectado ao servidor")
+}
+
+// quando recebe mensagem
+socket.onmessage = function(event) {
+    console.log("Mensagem recebida:", event.data)
+    alert("Mensagem recebida: " + event.data)
+}
+
+// quando desconecta
+socket.onclose = function() {
+    console.log("Conexão fechada")
+}
+
+// botão enviar
+function enviar() {
+    console.log("Enviando mensagem")
+    socket.send("Jogador clicou")
+}
